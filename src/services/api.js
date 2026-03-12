a3c8988a213ec44632a18b289ee06c1a1fb3f6e5
CHAMIKA DILSHAN HERATH <hmcdilshanherath@gmail.com>
2026-03-13 04:26:16 +0530

    c7

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
 const api = axios.create({
-  baseURL: `${baseUrl.replace(/\/$/, '')}/api`,
+  baseURL: `${baseUrl.replace(/\/+$/, '')}/api`,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
